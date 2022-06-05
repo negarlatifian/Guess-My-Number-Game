2a3f4238d1062af22b98d25a398e9436ffed0742
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-06-05 17:15:13 +0100

    button style added, android ripple added

--- guessNumGame/components/PrimaryButton.js
+++ guessNumGame/components/PrimaryButton.js
@@ -1,10 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 const PrimaryButton = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        android_ripple={{ color: "#640233" }}
+        style={styles.buttonInnerContainer}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
 
 export default PrimaryButton;
--- guessNumGame/screens/StartGameScreen.js
+++ guessNumGame/screens/StartGameScreen.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     elevation: 200,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
