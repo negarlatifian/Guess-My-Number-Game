13b4c304a45537c009b2c3e34e42b5b3867a1ca7
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-06-05 11:17:48 +0100

    screens added

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      Buttons
+    </View>
+  );
+};
+
+export default StartGameScreen;
