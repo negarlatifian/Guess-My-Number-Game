bb3136ab8903072d507444f893203dc2017b53ca
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-06-05 21:27:08 +0100

    started working on gameScreen

--- guessNumGame/screens/GameScreen.js
+++ guessNumGame/screens/GameScreen.js
@@ -1,6 +1,23 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      <View>
+        <Text>Log Rounds</Text>
+      </View>
+    </View>
+  );
 };
-
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 export default GameScreen;
