4d509276c8a39e61cc622c2334957f81b1ab51e1
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-06-06 18:07:28 +0100

    image added to GameOverScreen

--- guessNumGame/screens/GameOverScreen.js
+++ guessNumGame/screens/GameOverScreen.js
@@ -1,7 +1,35 @@
-import { Text } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 const GameOverScreen = () => {
-  return <Text>Game is Over!</Text>;
+  return (
+    <View>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.imageStyle}
+        />
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    borderRadius: 200,
+    width: 400,
+    height: 400,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default GameOverScreen;
